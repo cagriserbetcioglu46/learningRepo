31c99feaecad493758bc60942bf6881de4b14044
cagriserbetcioglu46 <serbetci199846@gmail.com>
2024-02-16 08:51:14 +0300

    ilk-asama

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component'; // Doğru yolu kullanın
+import { LeftSideNavbarComponent } from './left-side-navbar/left-side-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,LeftSideNavbarComponent], // NavbarComponent burada
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'my-first-project';
---
+++ src/app/left-side-navbar/left-side-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-side-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-side-navbar.component.html',
+  styleUrl: './left-side-navbar.component.scss'
+})
+export class LeftSideNavbarComponent {
+
+}
